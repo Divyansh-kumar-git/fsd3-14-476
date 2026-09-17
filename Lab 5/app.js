8ac066b1c77d68e11bee24b57c6c280de6003d6f
Divyansh Kumar <kumardivyansh474@gmail.com>
2026-09-17 15:40:56 +0530

    Add Lab 5

--- Lab 5/app.js
+++ Lab 5/app.js
@@ -6,4 +6,29 @@ app.get("/",(req,res)=> {
     res.send ("<h1> Hello Express");
 });
 
-app.listen (4000,()=> console.log ("Server is running "));
+app.get('/about',(req,res)=>{
+    res.send("We are FSD developer")
+});
+
+app.post('/login',(req,res)=>{
+    res.send({msg:'user login'})
+})
+
+app.put('/user/updates/1',(req,res)=>{
+    res.send({msg:'user update'})
+})
+
+app.delete('/users/1',(req,res)=>{
+    res.send({msg:'remove user 1'})
+})
+
+app.use((req,res)=>{
+    res.status(404).send("Not Found")
+})
+
+app.listen (3333,()=> console.log ("Server is running at 3333"));
+
+// ServiceWorkerRegistration.on("error", (err)=>{
+//     console.error("Server listen error:", err);
+// });
+
